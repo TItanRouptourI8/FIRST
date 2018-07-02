e57b968f379746b2e6b15a605da952dac9535167
Franck <unconfigured@null.spigotmc.org>
2018-07-03 00:16:00 +0200

    Ajout du "setDirection" pour tous les moteurs pour être sûr !

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareInit.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareInit.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import java.lang.reflect.Array;
@@ -11,8 +12,8 @@ public class HardwareInit
     private HardwareMap map;
 
 
-    public static  ArrayList<DcMotor> motors = new ArrayList<>();
-    public static int AVD = 0, AVG = 1, ARD = 2,ARG = 3, OUV = 4, MOISSONEUSE = 4;
+    private static  ArrayList<DcMotor> motors = new ArrayList<>();
+    static int AVD = 0, AVG = 1, ARD = 2,ARG = 3, OUV = 4, MOISSONEUSE = 4;
 
     private String[] motorsName = new String[] {"AVD","AVG","ARD","ARG","ROULETTE"};
     private int nbreMoteurs = motorsName.length;
@@ -20,7 +21,7 @@ public class HardwareInit
 
     //CONSTRUCTEUR
 
-    public HardwareInit(HardwareMap map)
+    HardwareInit(HardwareMap map)
     {
         this.map = map;
 
@@ -30,11 +31,12 @@ public class HardwareInit
 
     //INITIALISATION DES MOTEURS DANS L'ARRAY 'motors'
 
-    public void Initialisation()
+    private void Initialisation()
     {
         for (String name : motorsName)
         {
             DcMotor motor = map.dcMotor.get(name);
+            motor.setDirection(DcMotorSimple.Direction.FORWARD);
             motors.add(motor);
         }
 
