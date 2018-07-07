d123ad93a2bdc35e6ec5bb109a8f0e42ff5ae3f7
Franck <unconfigured@null.spigotmc.org>
2018-07-07 09:39:48 +0200

    Ajout du servo d'ouverture de porte

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareInit.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareInit.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -13,10 +14,12 @@ public class HardwareInit
 
 
     private static  ArrayList<DcMotor> motors = new ArrayList<>();
-    static int AVD = 0, AVG = 1, ARD = 2,ARG = 3, OUV = 4, MOISSONEUSE = 4,ASCG = 5, ASCD = 6;
+    static int AVD = 0, AVG = 1, ARD = 2,ARG = 3, MOISSONEUSE = 4,ASCG = 5, ASCD = 6;
 
     private String[] motorsName = new String[] {"AVD","AVG","ARD","ARG","ROULETTE","ASCG","ASCD"};
     private int nbreMoteurs = motorsName.length;
+    private Servo ouv;
+    public double posInit;
 
 
     //CONSTRUCTEUR
@@ -33,34 +36,47 @@ public class HardwareInit
 
     private void Initialisation()
     {
+        // Initialisation des moteurs DC placés dans un tableau
         for (String name : motorsName)
         {
             DcMotor motor = map.dcMotor.get(name);
-
+            motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
             motor.setDirection(DcMotor.Direction.FORWARD);
             motors.add(motor);
         }
-        // ATTENTION : l'un des moteurs ASC G ou D doit être mis en reverse
+
+
+        /*  Les deux moteurs de l'ascenseur sont montés dans des sens opposés
+            il faut qu'il y en ait un qui tourne à l'envers
+        */
+
         motors.get(ASCG).setDirection(DcMotor.Direction.REVERSE);
+
+        // Pour moissoner dans le sens positif, on retourne aussi le moteur
+
         motors.get(MOISSONEUSE).setDirection(DcMotor.Direction.REVERSE);
-        // *** Initialise les encodeurs des deux moteurs
+
+        // *** Initialise les encodeurs des deux moteurs de l'ascenseur
         motors.get(ASCG).setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motors.get(ASCD).setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-        // *** Indique au moteur gauche qu'il doit utiliser les encodeurs
-
-      //  motors.get(ASCG).setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        // *** Tentative simpliste d'asservissement du moteur droit :
+        /*  Pour synchroniser les moteurs, il suffit qu'ils soient en mode
+            RUN_TO_POSITION puis de leur donner la même position-cible et
+            la même vitesse.
+         */
 
         motors.get(ASCD).setMode(DcMotor.RunMode.RUN_TO_POSITION);
         motors.get(ASCG).setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        motors.get(ASCD).setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        motors.get(ASCG).setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
+        // initialisation  de la position cible à 0 qui est la position actuelle
         motors.get(ASCD).setTargetPosition(0);
         motors.get(ASCG).setTargetPosition(0);
+
+        // Initialisation du servo d'ouverture de porte
+
+        ouv = map.servo.get("OUV");
+        posInit = ouv.getPosition();
+
 
 
 
